f65de9e8daa520066811d73f2a11e74a69c3b2c0
gabiprds93 <gabiprds.93@gmail.com>
2017-07-25 15:19:37 -0500

    Estructura del js

---
+++ main.js
@@ -0,0 +1,24 @@
+function Tarea(userId, id, title, completed)
+{
+    this.userId = userId;
+    this.id = id;
+    this.title = title;
+    this.completed = completed;
+    this.mostrar = function()
+    {
+        
+    }
+}
+
+function ListaTareas()
+{
+    this.tareas = [];
+    this.agregar = function()
+    {
+        
+    }
+    this.editar = function()
+    {
+        
+    }
+}
