19750085870d9e17f8d3a6fa06db0a8445bb38f9
gabiprds93 <gabiprds.93@gmail.com>
2017-07-25 19:44:45 -0500

    Cambiando el texto de listas por input text

--- main.js
+++ main.js
@@ -4,15 +4,33 @@ function Tarea(title)
     this.completed = false;
     this.mostrar = function()
     {
-        var html = ""; 
-        html += "<li>" + this.title + "</li>";
-        document.getElementById("listaTareas").innerHTML += html;
+        var node = document.createElement("li");
+        var textnode = document.createTextNode(this.title);
+        node.appendChild(textnode);
+        var ul = document.getElementById("listaTareas");
+        ul.appendChild(node);
+        node.onclick = function()
+        {
+            var node1 = document.createElement("li");
+            var a = document.createElement("input");
+            a.type = "text";
+            a.value = textnode.nodeValue;
+            //var textnode1 = document.createTextNode("fdsfd");
+            node1.appendChild(a);
+            ul.appendChild(node1);
+            ul.replaceChild(node1, node);
+            node1.firstChild.focus();
+            //node.style.backgroundColor = "#ffd1a3";
+        };
     }
 }
 
 function ListaTareas()
 {
     this.tareas = [];
+
+    this.li = document.getElementsByTagName("li");
+    this.seleccion = undefined;
     this.agregar = function(tarea)
     {
         var inputTarea = document.getElementById("tarea");
@@ -25,10 +43,12 @@ function ListaTareas()
         this.tareas[this.tareas.length - 1].mostrar();
         inputTarea.value = "";
         inputTarea.focus();
+        console.log(this.li);
     }
+
     this.editar = function()
     {
-        
+                
     }
 }
 
@@ -38,7 +58,7 @@ listaTareas.agregar(new Tarea("Quis ut nam facilis et officia qui"));
 listaTareas.agregar(new Tarea("Fugiat veniam minus"));
 listaTareas.agregar(new Tarea("Et porro tempora"));
 listaTareas.agregar(new Tarea("Laboriosam mollitia et enim quasi adipisci quia provident illum"));
-listaTareas.agregar(new Tarea("Qui ullam ratione quibusdam voluptatem quia omnis"));
+listaTareas.agregar(new Tarea("Qui ullam ratione quilistaTareasdam voluptatem quia omnis"));
 listaTareas.agregar(new Tarea("Illo expedita consequatur quia in"));
 listaTareas.agregar(new Tarea("Quo adipisci enim quam ut ab"));
 listaTareas.agregar(new Tarea("Molestiae perspiciatis ipsa"));
